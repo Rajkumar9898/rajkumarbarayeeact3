a08f7a7234db705e3d1d2b6f9fb1fd37de8fd6a6
Rajkumar9898 <razchaurasia5@gmail.com>
2025-07-21 14:44:49 -0400

    added connection

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.set('views', path.join(__dirname, 'views'));
 const config = {
     user: 'azureuser',
     password: 'P@ssw0rdssss',
-    server: 'bingoone.database.windows.net',
-    database: 'sirsircost',
+    server: 'week115.database.windows.net',
+    database: 'week115',
     options: {
         encrypt: true,
         enableArithAbort: true
